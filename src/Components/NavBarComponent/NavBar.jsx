d33ab3ccadc5681509f7a58a473c935af9e81a05
olomonagas <olomonagas@gmail.com>
2025-10-28 20:06:27 -0300

    primera mejora

--- src/Components/NavBarComponent/NavBar.jsx
+++ src/Components/NavBarComponent/NavBar.jsx
@@ -2,32 +2,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  } from '@fortawesome/free-solid-svg-icons';
 
 function NavBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="text-white bg-dark">
       <Container>
-        <Navbar.Brand href="#home">Pizzeria Mamma Mia</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home" className='text-white'>Pizzeria Mamma Mia</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className='text-white' />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home" className='text-white'>Home</Nav.Link>
+            <Nav.Link href="#link" className='text-white'>Login</Nav.Link>
+            <Nav.Link href="#link" className='text-white'>Register</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    
   );
 }
 
@@ -60,3 +53,16 @@ export default NavBar;
     // </div>
 //   </nav> */}
     // )}
+
+    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" className='text-white'>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+
+//  <FontAwesomeIcon icon={byPrefixAndName.fat['lock']} />
+/* <FontAwesomeIcon icon={byPrefixAndName.fat['lock-open']} />*/
