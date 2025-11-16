8bda37e012764a97076cb4690f816b68595b76d7
olomonagas <olomonagas@gmail.com>
2025-11-16 19:57:34 -0300

    Mejoras en Home

--- src/Components/HomeComponent/Home.jsx
+++ src/Components/HomeComponent/Home.jsx
@@ -1,11 +1,20 @@
 import CardPizza from "../CardPizzaComponent/CardPizza";
 import Header from "../HeaderComponent/Header";
-import { TiposPizzas } from "../PizzaComponent/pizzas";
+// import { TiposPizzas } from "../PizzaComponent/pizzas";
+import { useState, useEffect } from "react";
+
 
 function Home() {
 
-const pizzas = TiposPizzas;
+  const [pizzas, setPizzas] = useState([]);
 
+  useEffect(() => {
+    
+    fetch("http://localhost:5000/api/pizzas")
+      .then((res) => res.json())
+      .then((data) => setPizzas(data))
+      .catch((err) => console.error("Error al cargar las pizzas:", err));
+  }, []);
 
   return (
     <>
